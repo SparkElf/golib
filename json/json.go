77f1a062bfe4ed172b550d35766ec829f1f385ea
SparkElf <374656045@qq.com>
2021-09-29 15:37:42 +0800

    master

---
+++ json/json.go
@@ -0,0 +1,31 @@
+package json
+
+import (
+	"bytes"
+
+	"encoding/json"
+	"strings"
+)
+
+func ToJson(s string) string {
+	var out bytes.Buffer
+	json.Indent(&out, []byte(s), "", "    ")
+	return out.String()
+}
+func Marshal(data interface{}) string {
+	b, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+func Unmarshal(s string) interface{} {
+	var result interface{}
+	d := json.NewDecoder(strings.NewReader(s))
+	d.UseNumber()
+	err := d.Decode(result)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
